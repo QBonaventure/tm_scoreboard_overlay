44b41e26d1a19ecd84718acb68018e4516776599
unknown <quentin@bonaventure.xyz>
2020-02-23 23:55:22 +0100

    Aranged a bit config files, created a dev.secret.exs.dist

--- config/dev.exs
+++ config/dev.exs
@@ -1,16 +1,5 @@
 use Mix.Config
 
-# Configure your database
-config :tm_scoreboard_overlay, TMSO.Repo,
-
-  username: "postgres",
-  password: "postgres",
-  database: "tmso_dev",
-  port: "5432",
-  hostname: "localhost",
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
-
 # For development, we disable any cache and enable
 # debugging and code reloading.
 #
